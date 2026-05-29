4704bb2bb3600d44f28fd5114c0e23b1af4be242
timurdevlikamov-cpu <timur.devlikamov@gmail.com>
2026-05-29 19:52:48 +0300

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -2,7 +2,7 @@
 docker network create --subnet=172.20.0.0/16 my_app_network
 
 #Запускаем контейнер с Mysql в сети 'my_app_network'. Благо вольюм сздается автоматически!
-docker run -d --name db_mysql --network my_app_network --ip 172.20.0.10 -e MYSQL_ROOT_PASSWORD=password mysql:8.0
+docker run -d --name db_mysql --network my_app_network --ip 172.20.0.10 -v 'db_data:/var/lib/mysql' --e 'MYSQL_ROOT_PASSWORD=YtReWq4321' -e 'MYSQL_DATABASE=virtd' -e 'MYSQL_USER=app' -e 'MYSQL_PASSWORD=QwErTy1234' mysql:8.0
 
 #Запускаем контейнер с wordpress в сети 'my_app_network'
 docker run -d --name web_python --network my_app_network --ip 172.20.0.5 -p 8080:5000 my-python-app
