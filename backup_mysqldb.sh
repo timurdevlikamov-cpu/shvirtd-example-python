5e6cb6b7b75b28c4e2ad616a99b6d75b5e106e26
timurdevlikamov-cpu <timur.devlikamov@gmail.com>
2026-06-03 22:23:05 +0300

    Update backup_mysqldb.sh

--- backup_mysqldb.sh
+++ backup_mysqldb.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+export $(grep -v '^#' .env | xargs)
+mkdir /opt/backup
 
+docker run --rm  --network "backend" -e MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD" -e MYSQL_DATABASE="$MYSQL_DATABASE" schnitzler/mysqldump mysqldump -h db-mysql -u root -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" > "$BACKUP_FILE"
